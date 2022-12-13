ebd4164f65ada966828823ac117c48d990caf990
Zuha-2109 <zoha.khundmiri2010@gmail.com>
2022-12-13 22:13:58 +0530

    Pizza1

--- src/main/java/com/driver/DeluxePizza.java
+++ src/main/java/com/driver/DeluxePizza.java
@@ -5,5 +5,19 @@ public class DeluxePizza extends Pizza {
     public DeluxePizza(Boolean isVeg) {
         super(isVeg);
         // your code goes here
+
+
+        //local not found automatically calls parent
+
+        addExtraCheese();
+        addExtraToppings();
+
+        //not right because it always go to parent class
+//        super.addExtraCheese();
+//        super.addTakeaway();
+//
+//        //local not found automatically calls parent
+//        this.addExtraCheese();
+//        this.addExtraToppings();
     }
 }
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,13 +6,20 @@ public class Main {
     System.out.println(dp.getPrice());
     dp.addTakeaway();
     dp.addExtraCheese();
+
+
     dp.addTakeaway();
     System.out.println(dp.getBill());
 
     Pizza p = new Pizza(true);
     p.addExtraCheese();
     p.addExtraToppings();
+
+
+
     p.addTakeaway();
+
+
     System.out.println(p.getBill());
   }
 }
